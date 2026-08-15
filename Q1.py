7b886ac0545ff17ca9169ffb116c460314032ee9
invasiono <122506260+invasiono@users.noreply.github.com>
2026-08-15 15:27:50 +0000

    Update

--- Q1.py
+++ Q1.py
@@ -1,6 +1,4 @@
 # ================ Code here =================
-print("\n[TEST]------------------------------------------------------------\n")
-
 class HotelRoom():
     def __init__(self, room_id, room_type, price_per_night, nights):
         self.room_id = str(room_id)
@@ -17,10 +15,6 @@ class HotelRoom():
     def display_info(self):
          return f"ID: {self.room_id} | Type: {self.room_type} | P/N: {self.price_per_night} | Nights: {self.nights}" 
 
-obj1 = HotelRoom("R000","Type",0,0)
-print(obj1.display_info())
-print(f"Price: {obj1.calculate_total()}")
-
 class StandardRoom(HotelRoom):
     def __init__(self, room_id, price_per_night, nights):
         super().__init__(room_id, "StandardRoom", price_per_night, nights)
@@ -33,10 +27,6 @@ class StandardRoom(HotelRoom):
 
     def display_info(self):
          return f"ID: {self.room_id} | Type: {self.room_type} | P/N: {self.price_per_night} | Nights: {self.nights}" 
-
-obj2 = StandardRoom("R001",50000,2)
-print(obj2.display_info())
-print(f"Price: {obj2.calculate_total()}")
 
 class DeluxeRoom(HotelRoom):
     def __init__(self, room_id, price_per_night, nights, service_fee):
@@ -52,10 +42,6 @@ class DeluxeRoom(HotelRoom):
     def display_info(self):
          return f"ID: {self.room_id} | Type: {self.room_type} | P/N: {self.price_per_night} | Nights: {self.nights} | Service: {self.service_fee}" 
 
-obj3 = DeluxeRoom("R002",10000,3,35000)
-print(obj3.display_info())
-print(f"Price: {obj3.calculate_total()}")
-
 class SuiteRoom(HotelRoom):
     def __init__(self, room_id, price_per_night, nights, service_fee, luxury_tax):
         super().__init__(room_id, "SuiteRoom", price_per_night, nights)
@@ -70,13 +56,7 @@ class SuiteRoom(HotelRoom):
 
     def display_info(self):
          return f"ID: {self.room_id} | Type: {self.room_type} | P/N: {self.price_per_night} | Nights: {self.nights} | Service: {self.service_fee} | Luxury Tax: {self.luxury_tax}" 
-
-obj4 = SuiteRoom("R003",20000,2,15000,20000)
-print(obj4.display_info())
-print(f"Price: {obj4.calculate_total()}")
-
-print("\n[RESULT]------------------------------------------------------------\n")
-
+    
 # ============================================
 
 # ===============Do not edit the code below================
